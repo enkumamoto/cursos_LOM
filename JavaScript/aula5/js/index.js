ce7d7fb46d6ba5c0d03a76ec1d7aaa8e8388e2f5
ekumamoto <eiji.kumamoto@gmail.com>
2023-05-09 08:38:38 -0300

    new files

---
+++ JavaScript/aula5/js/index.js
@@ -0,0 +1,8 @@
+console.log('oi')
+
+console.log('Ola Mundo')
+console.log('Este conteudo sera exibido')
+alert('Meu nome eh Eiji Kumamoto') // o alert nao existe no node.js
+
+/* caso queiram ais seguranca, pode separar os arquivos de backend numa pasta diferente das de frontend
+e modificar o apontamento no html*/
