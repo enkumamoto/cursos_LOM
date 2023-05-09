d2a1e1e7f1b1335d90d6a4b2eba9596e286f63a2
ekumamoto <eiji.kumamoto@gmail.com>
2023-05-09 14:43:56 -0300

    new files

--- PythonRegex/aula3.py
+++ PythonRegex/aula3.py
@@ -2,8 +2,13 @@
 # * representa 0 ou n
 # + representa 1 ou n
 # ? representa 0 ou 1
-# {n}
+# ()+ [a-zA-Z0-9]
+# {n} especificamente a quantidade desejada
 # {min, max}
+## {n, } de n ao max
+## {, n} ate o valor de n
+## {x,n} de x ate n
+
 
 import re
 
@@ -16,9 +21,30 @@ domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso
 pão de queijo.
 Não canso de ouvir a Maria:
 "Joooooooooãooooooo, o café tá prontinho aqui. Veeemm"!
+Jão
 '''
 
 print(re.findall(r'jOãO', texto, flags=re.I)) # acha os dois joao, mas nao o ultimo
 print("---")
 
-print(re.findall(r'jo+ão', texto, flags=re.I))
+print(re.findall(r'jo+ão+', texto, flags=re.I)) # o quantificador busca o que esta a esquerda dele
+print("---")
+
+# substituindo palavras dentro do texto
+print(re.sub(r'jo+ão+', 'Felipe', texto, flags=re.I)) # So altera a saida
+print("---")
+
+print(re.sub(r'jo*ão*', 'Felipe', texto, flags=re.I)) # So altera a saida
+print("---")
+
+print(re.sub(r'jo?ão*', 'Felipe', texto, flags=re.I)) # So altera a saida mas mantem o Joooooooooãooooooo
+print("---")
+
+# exemplificando a captura de quantidade especifica de repeticao
+print(re.findall(r've{3}m{1,2}', texto, flags=re.I))
+print("---")
+
+# exemplificando range
+texto2 = 'ele ama ser amado'
+
+print(re.findall(r'ama[do]{0,2}', texto2, flags=re.I))
