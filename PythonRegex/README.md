93144170ae39cda05dfac865ec62beff0ffec742
ekumamoto <eiji.kumamoto@gmail.com>
2023-05-09 15:27:17 -0300

    new files

--- PythonRegex/README.md
+++ PythonRegex/README.md
@@ -1,2 +1,3 @@
 # python_regex
 # Iniciando python com regex
+# Curso eh gratuito na udemy, entao nao tem certificado
---
+++ PythonRegex/aula4.py
@@ -0,0 +1,25 @@
+## quantificadore grid e nao grid
+# Meta caracteres: . ^ $ ( )
+# * representa 0 ou n
+# + representa 1 ou n
+# ? representa 0 ou 1
+
+
+
+import re
+
+texto = '''
+<p>Frase 1</p> <p>Eita</p> <p>Qualquer frase</p> <div></div> 
+'''
+
+print(re.findall(r'<[pdiv]{1,3}>', texto))
+print("---")
+
+print(re.findall(r'<[pdiv]{1,3}>.*', texto)) # aqui pega tudo
+print("---")
+
+print(re.findall(r'<[pdiv]{1,3}>.*<\/[pdiv]{1,3}>', texto)) # comportamento grid
+print("---")
+
+print(re.findall(r'<[pdiv]{1,3}>.*?<\/[pdiv]{1,3}>', texto)) # modificando o comportamento para non grid
+print("---")
